9e0a60bca95448895f7357fc1a9c2f86483f6663
Frassi3557 <freysi_freys@live.com>
2019-05-08 17:46:11 +0000

    Add files via upload

---
+++ Verkefni 3/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+
+    private Vector3 offset;
+
+    public float camPosX;
+    public float camPosY;
+    public float camPosZ;
+
+    public float camRotationX;
+    public float camRotationY;
+    public float camRotationZ;
+
+    public float turnSpeed;
+
+    void Start()
+    {
+        offset = new Vector3(player.position.x - camPosX, player.position.y - camPosY, player.position.z - camPosZ);
+        transform.rotation = Quaternion.Euler(camRotationX, camRotationY, camRotationZ);
+    }
+
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.left) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
